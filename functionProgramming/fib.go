f19e916970d563ffe35fc71008967092f6273393
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2019-01-29 22:05:03 -0600

    functions

---
+++ functionProgramming/fib.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func fib() func() int {
+    a, b := 0, 1
+    return func() int {
+        a, b := b, a + b
+        return a
+    }
+}
+
+func main() {
+    f := fib()
+
+    fmt.Println(f())
+    fmt.Println(f())
+    fmt.Println(f())
+    fmt.Println(f())
+    fmt.Println(f())
+    fmt.Println(f())
+    fmt.Println(f())
+    fmt.Println(f())
+
+}
